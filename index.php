ad415e1eebda762f3242a5b117842101b431f578
Richard Klein <code@rklein.me>
2013-10-25 11:35:05 +0200

    Split into functions, uses  and can change the lines threshold

--- index.php
+++ index.php
@@ -28,11 +28,13 @@ class graph{
         var_dump($this->adj);
     }
 
-    public function dot(){
+    public function dot($lines){
         $text  = "strict graph myGraph {\nsize=\"(20,100)\";\n ";
         foreach($this->adj as $s => $d){
             foreach($d as $v => $w){
-                $text .= "\t\t\"" . $s . "\" -- \"" . $v . "\";\n";
+                if($w >= $lines){
+                    $text .= "\t\t\"" . $s . "\" -- \"" . $v . "\"[label=$w];\n";
+                }
             }
 
         }
@@ -41,53 +43,84 @@ class graph{
         return $text;
     }
 
-    public function image(){
-        $text = $this->dot();
+    public function image($code, $lines ){
+        $text = $this->dot($lines);
 
-        unlink("/tmp/mossp.dot");
-        unlink("/tmp/mossp.png");
-        file_put_contents ("/tmp/mossp.dot", $text);
+        unlink("/tmp/moss/$code.dot");
+        unlink("/tmp/moss/$code.png");
+        file_put_contents ("/tmp/moss/$code.dot", $text);
 
-        exec("unflatten -f  -l 100 /tmp/mossp.dot | /usr/bin/dot -Tpng -o /tmp/mossp.png");
+        exec("unflatten -f  -l 100 /tmp/moss/$code.dot | /usr/bin/dot -Tpng -o /tmp/moss/$code.png");
+
+        return "/tmp/moss/$code.png";
     }
 
 
 }
 
-$doc = new DOMDocument();
+function showMoss($result, $lines){
+    $doc = new DOMDocument();
 
-$source = file_get_contents("results.html");
-$doc->loadHTML($source);
-
-$table = $doc->getElementsByTagName("table");
-$table = $table->item(0);
-
-$rows = $table->getElementsByTagName("tr");
-
-$linematches = new graph();
-for ($i = 1; $i < $rows->length; $i++){
-    $row = $rows->item($i);
-    $cols = $row->getElementsByTagName("td");
-
-    $name1 = $cols->item(0)->nodeValue;
-    $name2 = $cols->item(1)->textContent;
-    $linem = $cols->item(2)->textContent;
+    $context = array(
+        'http' => array(
+                'proxy' => 'tcp://127.0.0.1:3128',
+                'request_fulluri' => true,
+        ),
     
-    $pattern = "|\./|";
-    $name1 = trim(preg_replace($pattern, "", $name1));
-    $name2 = trim(preg_replace($pattern, "", $name2));
-    $pattern = "| \(..*%\)|";
-    $name1 = trim(preg_replace($pattern, "", $name1));
-    $name2 = trim(preg_replace($pattern, "", $name2));
-    $linem = trim($linem);
+    );
+    $sContext = stream_context_create($context);
 
-    $linematches->addEdge($name1, $name2, $linem);
+    $source = file_get_contents("http://moss.stanford.edu/results/$result/", false, $sContext);
+    //$source = file_get_contents("results.html");
+    $doc->loadHTML($source);
+
+    $table = $doc->getElementsByTagName("table");
+    $table = $table->item(0);
+    
+    $rows = $table->getElementsByTagName("tr");
+    
+    $linematches = new graph();
+    for ($i = 1; $i < $rows->length; $i++){
+        $row = $rows->item($i);
+        $cols = $row->getElementsByTagName("td");
+    
+        $name1 = $cols->item(0)->nodeValue;
+        $name2 = $cols->item(1)->textContent;
+        $linem = $cols->item(2)->textContent;
+        
+        $pattern = "|\./|";
+        $name1 = trim(preg_replace($pattern, "", $name1));
+        $name2 = trim(preg_replace($pattern, "", $name2));
+        $pattern = "| \(..*%\)|";
+        $name1 = trim(preg_replace($pattern, "", $name1));
+        $name2 = trim(preg_replace($pattern, "", $name2));
+        $linem = trim($linem);
+    
+        $linematches->addEdge($name1, $name2, $linem);
+    }
+    
+    $filename = $linematches->image($result, $lines);
+    header("Content-type: image/png");
+    //$image=imagecreatefromjpeg($_GET['img']);
+    //imagejpeg($image);
+    readfile($filename);
 }
 
-$linematches->image();
-header("Content-type: image/png");
-//$image=imagecreatefromjpeg($_GET['img']);
-//imagejpeg($image);
-readfile("/tmp/mossp.png");
+function getMossNumber(){
+    echo "Moss Number Required";
+
+}
+
+$lines = 0;
+if(isset($_GET['lines'])){
+    $lines = $_GET['lines'];
+}
+
+if(isset($_GET['moss'])){
+    showMoss($_GET['moss'], $lines);
+}else{
+    getMossNumber();
+}
+
 ?>
 
